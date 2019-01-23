524810a9dfd3159a7d4c161d96aba666c2b88c83
carlosviana <viana.mail@gmail.com>
2019-01-23 21:47:51 -0200

    fim

--- goreacmod4/src/components/Player/index.js
+++ goreacmod4/src/components/Player/index.js
@@ -26,10 +26,31 @@ import PauseIcon from "../../assets/images/pause.svg";
 import ForwardIcon from "../../assets/images/forward.svg";
 import RepeatIcon from "../../assets/images/repeat.svg";
 
-const Player = ({ player, play, pause }) => (
+const Player = ({
+    player,
+    play,
+    pause,
+    next,
+    prev,
+    playing,
+    position,
+    duration,
+    positionShow,
+    progress,
+    handlePosition,
+    setPosition,
+    setVolume
+}) => (
     <Container>
         {!!player.currentSong && (
-            <Sound url={player.currentSong.file} playStatus={player.status} />
+            <Sound
+                url={player.currentSong.file}
+                playStatus={player.status}
+                onFinishedPlaying={next}
+                onPlaying={playing}
+                position={player.position}
+                volume={player.volume}
+            />
         )}
         <Current>
             {!!player.currentSong && (
@@ -51,8 +72,8 @@ const Player = ({ player, play, pause }) => (
                 <button>
                     <img src={ShuffleIcon} alt="Shuffle" />
                 </button>
-                <button>
-                    <img src={Backwardcon} alt="Shuffle" />
+                <button onClick={prev}>
+                    <img src={Backwardcon} alt="Backward" />
                 </button>
                 {!!player.currentSong &&
                 player.status === Sound.status.PLAYING ? (
@@ -64,7 +85,7 @@ const Player = ({ player, play, pause }) => (
                         <img src={PlayIcon} alt="Shuffle" />
                     </button>
                 )}
-                <button>
+                <button onClick={next}>
                     <img src={ForwardIcon} alt="Shuffle" />
                 </button>
                 <button>
@@ -73,15 +94,19 @@ const Player = ({ player, play, pause }) => (
             </Controls>
 
             <Time>
-                <span>1:39</span>
+                <span>{positionShow || position}</span>
                 <ProgressSlider>
                     <Slider
                         railStyle={{ background: "#404040", borderRadius: 10 }}
                         trackStyle={{ background: "#1ED760" }}
                         handleStyle={{ border: 0 }}
+                        max={1000}
+                        onChange={value => handlePosition(value / 1000)}
+                        onAfterChange={value => setPosition(value / 1000)}
+                        value={progress}
                     />
                 </ProgressSlider>
-                <span>4:55</span>
+                <span>{duration}</span>
             </Time>
         </Progress>
 
@@ -91,7 +116,8 @@ const Player = ({ player, play, pause }) => (
                 railStyle={{ background: "#404040", borderRadius: 10 }}
                 trackStyle={{ background: "#FFF" }}
                 handleStyle={{ display: "none" }}
-                value={100}
+                value={player.volume}
+                onChange={setVolume}
             />
         </Volume>
     </Container>
@@ -108,11 +134,40 @@ Player.propTypes = {
         })
     }).isRequired,
     play: PropTypes.func.isRequired,
-    pause: PropTypes.func.isRequired
+    pause: PropTypes.func.isRequired,
+    next: PropTypes.func.isRequired,
+    prev: PropTypes.func.isRequired,
+    playing: PropTypes.func.isRequired,
+    handlePosition: PropTypes.func.isRequired,
+    setPosition: PropTypes.func.isRequired,
+    position: PropTypes.string.isRequired,
+    duration: PropTypes.string.isRequired,
+    positionShow: PropTypes.string.isRequired,
+    setVolume: PropTypes.func.isRequired
 };
 
+function msToTime(duration) {
+    if (!duration) return null;
+
+    let seconds = parseInt((duration / 1000) % 60, 10);
+    let minutes = parseInt((duration / (1000 * 60)) % 60, 10);
+
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    return `${minutes}:${seconds}`;
+}
+
 const mapStateToProps = state => ({
-    player: state.player
+    player: state.player,
+    position: msToTime(state.player.position),
+    duration: msToTime(state.player.duration),
+    positionShow: msToTime(state.player.positionShow),
+    progress:
+        parseInt(
+            (state.player.positionShow || state.player.position) *
+                (1000 / state.player.duration),
+            10
+        ) || 0
 });
 
 const mapDispatchToProps = dispatch =>
--- goreacmod4/src/store/ducks/player.js
+++ goreacmod4/src/store/ducks/player.js
@@ -3,12 +3,23 @@ import Sound from "react-sound";
 export const Types = {
     LOAD: "player/LOAD",
     PLAY: "palyer/PLAY",
-    PAUSE: "player/PAUSE"
+    PAUSE: "player/PAUSE",
+    NEXT: "player/NEXT",
+    PREV: "player/PREV",
+    PLAYING: "player/PLAYING",
+    HANDLE_POSITION: "player/HANDLE_POSITION",
+    SET_POSITION: "player/SET_POSITION",
+    SET_VOLUME: "player/SET_VOLUME"
 };
 
 const INITIAL_STATE = {
     currentSong: null,
-    status: Sound.status.PLAYING
+    list: [],
+    status: Sound.status.PLAYING,
+    position: null,
+    duration: null,
+    positionShow: null,
+    volume: 100
 };
 
 export default function player(state = INITIAL_STATE, action) {
@@ -17,6 +28,7 @@ export default function player(state = INITIAL_STATE, action) {
             return {
                 ...state,
                 currentSong: action.payload.song,
+                list: action.payload.list,
                 status: Sound.status.PLAYING
             };
         case Types.PLAY:
@@ -29,16 +41,86 @@ export default function player(state = INITIAL_STATE, action) {
                 ...state,
                 status: Sound.status.PAUSE
             };
+        case Types.NEXT: {
+            const currentIndex = state.list.findIndex(
+                song => song.id === state.currentSong.id
+            );
+            const next = state.list[currentIndex + 1];
+
+            if (next) {
+                return {
+                    ...state,
+                    currentSong: next,
+                    status: Sound.status.PLAYING,
+                    position: 0
+                };
+            }
+
+            return state;
+        }
+        case Types.PREV: {
+            const currentIndex = state.list.findIndex(
+                song => song.id === state.currentSong.id
+            );
+            const prev = state.list[currentIndex - 1];
+
+            if (prev) {
+                return {
+                    ...state,
+                    currentSong: prev,
+                    status: Sound.status.PLAYING,
+                    position: 0
+                };
+            }
+
+            return state;
+        }
+        case Types.PLAYING:
+            return { ...state, ...action.payload };
+        case Types.HANDLE_POSITION:
+            return {
+                ...state,
+                positionShow: state.duration * action.payload.percent
+            };
+        case Types.SET_POSITION:
+            return {
+                ...state,
+                position: state.duration * action.payload.percent,
+                positionShow: null
+            };
+        case Types.SET_VOLUME:
+            return {
+                ...state,
+                volume: action.payload.volume
+            };
         default:
             return state;
     }
 }
 
 export const Creators = {
-    loadSong: song => ({
+    loadSong: (song, list) => ({
         type: Types.LOAD,
-        payload: { song }
+        payload: { song, list }
     }),
     play: () => ({ type: Types.PLAY }),
-    pause: () => ({ type: Types.PAUSE })
+    pause: () => ({ type: Types.PAUSE }),
+    next: () => ({ type: Types.NEXT }),
+    prev: () => ({ type: Types.PREV }),
+    playing: ({ position, duration }) => ({
+        type: Types.PLAYING,
+        payload: { position, duration }
+    }),
+    handlePosition: percent => ({
+        type: Types.HANDLE_POSITION,
+        payload: { percent }
+    }),
+    setPosition: percent => ({
+        type: Types.SET_POSITION,
+        payload: { percent }
+    }),
+    serVolume: volume => ({
+        type: Types.SET_VOLUME,
+        payload: { volume }
+    })
 };
